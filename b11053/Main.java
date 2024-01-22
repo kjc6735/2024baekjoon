c19764ab51ae5f364f8303ccdf5e1823a7d4cdc3
jaechan <penguin_03@naver.com>
2024-01-22 23:51:14 +0900

    ㅅㄷㄴㅅ

--- b11053/Main.java
+++ b11053/Main.java
@@ -1,54 +1,31 @@
-//package b11053;
-//
-//import java.io.BufferedReader;
-//import java.io.FileInputStream;
-//import java.io.InputStreamReader;
-//
-//public class Main {
-//    static BufferedReader br;
-//    static int dp[][][];
-//    public static void main(String[] args)throws Exception {
-//        System.setIn(new FileInputStream("b11053/input.txt"));
-//        br = new BufferedReader(new InputStreamReader(System.in));
-//        int n = Integer.parseInt(br.readLine());
-//        int arr[] = new int[n+1];
-//        String str[] = br.readLine().split(" ");
-//        for(int i =0; i < n; i++) arr[i] = Integer.parseInt(str[i]);
-//        print(n+1);
-//        dp= new int[n+1][n+1][2];
-//        for(int i = 1; i < n ; i++){
-//            if(arr[i-1] < arr[i]){
-//                dp[0][i][0] = dp[i][0][0] =  arr[i];
-//                dp[0][i][1] = dp[0][i][1] = dp[0][i -1][1] + 1;
-//            }else {
-//                dp[0][i][0] = dp[i][0][0] = dp[i-1][0][0];
-//                dp[0][i][1] = dp[0][i][1] = dp[i-1][0][1];
-//            }
-//        }
-//
-//
-//        for(int i = 0 ; i < n ; i++){
-//            for(int k =i+1; k < n ;k++){
-//                if(arr[i] > arr[k]){
-//                    dp[i+1][k+1][0] = arr[i];
-//                    dp[i+1][k+1][1] = dp[i][k][1] + 1;
-//                    dp[k+1][i+1][0] = arr[i];
-//                    dp[k+1][i+1][1] = dp[i][k][1] + 1;
-//                }else {
-//                    dp[i+1][k+1][0] = dp[i][k-1][0];
-//                    dp[i+1][k+1][1] = dp[i][k][1];
-//                }
-//            }
-//        }
-//        System.out.println(dp[n][n][0]);
-//    }
-//
-//    static void print(int n){
-//        for(int i =0 ; i < n ; i++){
-//            for(int k = 0; k < n ; k++){
-//                System.out.print(dp[i][k][0] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-//}
+package b11053;
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static BufferedReader br;
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("b11053/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n ; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        for(int i = 1; i <= n ; i++ ){
+            dp[i] = 1;
+            for(int j = 1; j < i; j++){
+                if(arr[i] > arr[j]) {
+                    if(dp[i] < (dp[j]+1)) dp[i] = dp[j] + 1;
+                }
+            }
+            if(ans < dp[i]) ans = dp[i];
+        }
+        System.out.println(ans);
+    }
+
+}
